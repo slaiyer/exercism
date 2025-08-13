39dd429966048f79b553c54a286a3430dd13c24e
slaiyer <1776970+slaiyer@users.noreply.github.com>
2025-08-14 02:14:32 +0530

    improve clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
-const DAY: i64 = 24 * 60;
-const HOUR: i64 = 60;
+const MINS_HOUR: i64 = 60;
+const MINS_DAY: i64 = 24 * MINS_HOUR;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Clock {
@@ -9,19 +9,24 @@ pub struct Clock {
 }
 
 impl Clock {
-    pub fn new(hours: i64, minutes: i64) -> Clock {
-        Clock {
-            minutes: (((hours * HOUR + minutes) % DAY) + DAY) % DAY,
+    pub fn new(hours: i64, minutes: i64) -> Self {
+        Self {
+            minutes: (((hours * MINS_HOUR + minutes) % MINS_DAY) + MINS_DAY) % MINS_DAY,
         }
     }
 
-    pub fn add_minutes(self, minutes: i64) -> Clock {
-        Clock::new(0, self.minutes + minutes)
+    pub fn add_minutes(self, minutes: i64) -> Self {
+        Self::new(0, self.minutes + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.minutes / HOUR, self.minutes % HOUR)
+        write!(
+            f,
+            "{:02}:{:02}",
+            self.minutes / MINS_HOUR,
+            self.minutes % MINS_HOUR
+        )
     }
 }
